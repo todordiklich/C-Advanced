a8c91bf666f2a1852c8bebd7c47b1f09f389d1ab
Todor Diklich <todordiklich@gmail.com>
2020-09-23 21:57:57 +0300

    Adding solution to problems 8 and 9

---
+++ Advanced/Multidimensional-Arrays-Exercises/8. Bombs/Program.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Linq;
+
+namespace _8._Bombs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[,] matrix = CreateMatrix(n, n);
+
+            string[] bombs = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+            for (int i = 0; i < bombs.Length; i++)
+            {
+                int[] bombCoordinates = bombs[i].Split(',', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+                int bombRow = bombCoordinates[0];
+                int bombCol = bombCoordinates[1];
+
+                // destroing cells in the matrix
+                if (ValidateCoordinates(bombRow, bombCol, matrix) && matrix[bombRow, bombCol] > 0)
+                {
+                    int bombValue = matrix[bombRow, bombCol];
+                    matrix[bombRow, bombCol] = 0;
+                    for (int r = bombRow - 1; r < bombRow + 2; r++)
+                    {
+                        for (int c = bombCol - 1; c < bombCol + 2; c++)
+                        {
+                            if (ValidateCoordinates(r, c, matrix) && matrix[r, c] > 0)
+                            {
+                                matrix[r, c] -= bombValue;
+                            }
+                        }
+                    }
+                }
+            }
+
+            // searching for alive cells
+            int aliveSum = 0;
+            int numberOfAliveCells = 0;
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    if (matrix[row, col] > 0)
+                    {
+                        aliveSum += matrix[row, col];
+                        numberOfAliveCells++;
+                    }
+                }
+            }
+
+            Console.WriteLine($"Alive cells: {numberOfAliveCells}");
+            Console.WriteLine($"Sum: {aliveSum}");
+            PrintMatrix(matrix);
+        }
+
+        static int[,] CreateMatrix(int rows, int cols)
+        {
+            int[,] matrix = new int[rows, cols];
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                int[] arr = Console
+                    .ReadLine()
+                    .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse)
+                    .ToArray();
+
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = arr[col];
+                }
+            }
+
+            return matrix;
+        }
+
+        static void PrintMatrix(int[,] matrix)
+        {
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    Console.Write(matrix[row, col] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        static bool ValidateCoordinates(int row, int col, int[,] matrix)
+        {
+            bool areInside = true;
+
+            if (row < 0 || row >= matrix.GetLength(0) ||
+                col < 0 || col >= matrix.GetLength(1))
+            {
+                areInside = false;
+            }
+
+            return areInside;
+        }
+    }
+}
---
+++ Advanced/Multidimensional-Arrays-Exercises/9. Miner/Program.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Linq;
+
+namespace _9._Miner
+{
+    class Program
+    {
+        private static int currRow;
+        private static int currCol;
+        private static int totalCoals;
+
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            string[] directions = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            char[,] matrix = CreateMatrix(n, n);
+
+            GetStartingPosition(matrix);
+            totalCoals = GetTotalCoalsNumber(matrix);
+
+            for (int i = 0; i < directions.Length; i++)
+            {
+                if (directions[i] == "left")
+                {
+                    StepOnCell(currRow, currCol - 1, matrix);
+                }
+                else if (directions[i] == "right")
+                {
+                    StepOnCell(currRow, currCol + 1, matrix);
+                }
+                else if (directions[i] == "up")
+                {
+                    StepOnCell(currRow - 1, currCol, matrix);
+                }
+                else if (directions[i] == "down")
+                {
+                    StepOnCell(currRow + 1, currCol, matrix);
+                }
+            }
+
+            Console.WriteLine($"{totalCoals} coals left. ({currRow}, {currCol})");
+        }
+
+        static char[,] CreateMatrix(int rows, int cols)
+        {
+            char[,] matrix = new char[rows, cols];
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                char[] arr = Console
+                    .ReadLine()
+                    .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                    .Select(char.Parse)
+                    .ToArray();
+
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = arr[col];
+                }
+            }
+
+            return matrix;
+        }
+
+        static bool ValidateCoordinates(int row, int col, char[,] matrix)
+        {
+            bool areInside = true;
+
+            if (row < 0 || row >= matrix.GetLength(0) ||
+                col < 0 || col >= matrix.GetLength(1))
+            {
+                areInside = false;
+            }
+
+            return areInside;
+        }
+
+        static int GetTotalCoalsNumber(char[,] matrix)
+        {
+            int coals = 0;
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    if (matrix[row, col] == 'c')
+                    {
+                        coals++;
+                    }
+                }
+            }
+
+            return coals;
+        }
+
+        static void GetStartingPosition(char[,] matrix)
+        {
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    if (matrix[row, col] == 's')
+                    {
+                        currRow = row;
+                        currCol = col;
+                    }
+                }
+            }
+        }
+
+        static void StepOnCell(int row, int col, char[,] matrix)
+        {
+            if (ValidateCoordinates(row, col, matrix))
+            {
+                currRow = row;
+                currCol = col;
+
+                if (matrix[row, col] == 'c')
+                {
+                    matrix[row, col] = '*';
+                    totalCoals--;
+
+                    if (totalCoals == 0)
+                    {
+                        Console.WriteLine($"You collected all coals! ({currRow}, {currCol})");
+
+                        Environment.Exit(0);
+                    }
+                }
+                else if (matrix[row, col] == 'e')
+                {
+                    Console.WriteLine($"Game over! ({currRow}, {currCol})");
+
+                    Environment.Exit(0);
+                }
+            }
+        }
+    }
+}
