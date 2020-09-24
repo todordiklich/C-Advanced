7ad5105fc731da6e7399d45d76aa475020972fe0
Todor Diklich <todordiklich@gmail.com>
2020-09-24 21:22:11 +0300

    Adding solution to problem 10

--- Advanced/Multidimensional-Arrays-Exercises/10. Radioactive Mutant Vampire Bunnies/Program.cs
+++ Advanced/Multidimensional-Arrays-Exercises/10. Radioactive Mutant Vampire Bunnies/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace _10._Radioactive_Mutant_Vampire_Bunnies
@@ -9,6 +10,7 @@ namespace _10._Radioactive_Mutant_Vampire_Bunnies
         private static int currCol;
         private static bool hasWon = false;
         private static bool hasDied = false;
+        private static List<int> bunniesPositions = new List<int>();
         static void Main(string[] args)
         {
             int[] input = Console
@@ -46,18 +48,79 @@ namespace _10._Radioactive_Mutant_Vampire_Bunnies
                     MovePlayer(currRow + 1, currCol, matrix);
                 }
 
-                //spread bunnies
+                //find bunnies positions & spread bunnies
+                GetBunniesPositions(matrix);
+                SpreadBunnies(matrix);
 
                 //check if player won/died
                 if (hasWon)
                 {
-                    Console.WriteLine($"won: {currRow} {currCol}");
                     break;
                 }
                 if (hasDied)
                 {
-                    Console.WriteLine($"dead: {currRow} {currCol}");
                     break;
+                }
+            }
+
+            PrintMatrix(matrix);
+            Console.WriteLine(hasWon == true ? $"won: {currRow} {currCol}" : $"dead: {currRow} {currCol}");
+        }
+
+        static void PrintMatrix(char[,] matrix)
+        {
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    Console.Write(matrix[row, col]);
+                }
+                Console.WriteLine();
+            }
+        }
+
+        static void SpreadBunnies(char[,] matrix)
+        {
+            for (int i = 0; i < bunniesPositions.Count - 1; i += 2)
+            {
+                int row = bunniesPositions[i];
+                int col = bunniesPositions[i + 1];
+
+                //lef
+                PlaceBunny(row, col - 1, matrix);
+                //right
+                PlaceBunny(row, col + 1, matrix);
+                //up
+                PlaceBunny(row - 1, col, matrix);
+                //down
+                PlaceBunny(row + 1, col, matrix);
+            }
+        }
+
+        private static void PlaceBunny(int row, int col, char[,] matrix)
+        {
+            if (ValidateCoordinates(row, col, matrix))
+            {
+                if (matrix[row, col] == 'P')
+                {
+                    hasDied = true;
+                }
+
+                matrix[row, col] = 'B';
+            }
+        }
+
+        static void GetBunniesPositions(char[,] matrix)
+        {
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    if (matrix[row, col] == 'B')
+                    {
+                        bunniesPositions.Add(row);
+                        bunniesPositions.Add(col);
+                    }
                 }
             }
         }
@@ -78,6 +141,7 @@ namespace _10._Radioactive_Mutant_Vampire_Bunnies
 
             return matrix;
         }
+
         static bool ValidateCoordinates(int row, int col, char[,] matrix)
         {
             bool areInside = true;
@@ -110,10 +174,11 @@ namespace _10._Radioactive_Mutant_Vampire_Bunnies
         {
             if (ValidateCoordinates(row, col, matrix))
             {
+                currRow = row;
+                currCol = col;
+
                 if (matrix[row, col] != 'B')
                 {
-                    currRow = row;
-                    currCol = col;
                     matrix[currRow, currCol] = 'P';
                 }
                 else
