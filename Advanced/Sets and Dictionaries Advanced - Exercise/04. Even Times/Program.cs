c49ed38d8636ebb18701f187c47b3d101c63fbbb
todordiklich <todordiklich@gmail.com>
2020-09-29 16:30:02 +0300

    Adding solutions to exercise Sets and Dictionaries

---
+++ Advanced/Sets and Dictionaries Advanced - Exercise/04. Even Times/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _04._Even_Times
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int, int> numbers = new Dictionary<int, int>();
+
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                int num = int.Parse(Console.ReadLine());
+
+                if (!numbers.ContainsKey(num))
+                {
+                    numbers.Add(num, 0);
+                }
+
+                numbers[num]++;
+            }
+
+            foreach (var item in numbers)
+            {
+                if (item.Value % 2 == 0)
+                {
+                    Console.WriteLine(item.Key);
+                }
+            }
+        }
+    }
+}
---
+++ Advanced/Sets and Dictionaries Advanced - Exercise/05. Count Symbols/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _05._Count_Symbols
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<char, int> numbers = new Dictionary<char, int>();
+
+            string text = Console.ReadLine();
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                char ch = text[i];
+
+                if (!numbers.ContainsKey(ch))
+                {
+                    numbers.Add(ch, 0);
+                }
+
+                numbers[ch]++;
+            }
+
+            foreach (var item in numbers.OrderBy(e => e.Key))
+            {
+                Console.WriteLine($"{item.Key}: {item.Value} time/s");
+            }
+        }
+    }
+}
