ca4167e1f9f89839e3d613f46f6ea76c3ac45298
todordiklich <todordiklich@gmail.com>
2020-10-02 12:41:26 +0300

    Adding solutions to all the Lab problems

---
+++ Advanced/Streams, Files and Directories - Lab/Streams, Files and Directories - Lab/04. Merge Files/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+
+namespace _04._Merge_Files
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var firstReader = new StreamReader("../../../FileOne.txt"))
+            {
+                using (var secondReader = new StreamReader("../../../FileTwo.txt"))
+                {
+                    using (var writer = new StreamWriter("../../../Otput.txt"))
+                    {
+                        string firstLine = firstReader.ReadLine();
+                        string secondLine = secondReader.ReadLine();
+
+                        while (firstLine != null || secondLine != null)
+                        {
+                            if (firstLine != null)
+                            {
+                                writer.WriteLine(firstLine);
+                            }
+                            if (secondLine != null)
+                            {
+                                writer.WriteLine(secondLine);
+                            }
+
+                            firstLine = firstReader.ReadLine();
+                            secondLine = secondReader.ReadLine();
+                        }
+                    }
+                }
+
+            }
+        }
+    }
+}
---
+++ Advanced/Streams, Files and Directories - Lab/Streams, Files and Directories - Lab/05. Slice File/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace _05._Slice_File
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int parts = 4;
+
+            using (var reader = new FileStream("../../../sliceMe.txt", FileMode.Open))
+            {
+                long pieceSize = (long)Math.Ceiling((double)reader.Length / parts);
+
+                for (int i = 0; i < parts; i++)
+                {
+                    long currPieceSize = 0;
+
+                    using (var createFile = new FileStream($"../../../Part-{i+1}.txt", FileMode.Create))
+                    {
+                        byte[] buffer = new byte[4096];
+
+                        while (reader.Read(buffer, 0, buffer.Length) == buffer.Length)
+                        {
+                            currPieceSize += buffer.Length;
+                            createFile.Write(buffer, 0, buffer.Length);
+
+                            if (currPieceSize >= pieceSize) 
+                            { 
+                                break; 
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
