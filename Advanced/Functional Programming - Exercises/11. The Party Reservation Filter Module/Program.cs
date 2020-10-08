87415c5fdd64aadf00eb7b2c4d7791a30db0c48d
todordiklich <todordiklich@gmail.com>
2020-10-08 10:50:54 +0300

    Adding solutions to problems 11 and 12

--- Advanced/Functional Programming - Exercises/11. The Party Reservation Filter Module/Program.cs
+++ Advanced/Functional Programming - Exercises/11. The Party Reservation Filter Module/Program.cs
@@ -1,6 +1,8 @@
 ﻿
 
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace _11._The_Party_Reservation_Filter_Module
 {
@@ -8,7 +10,62 @@ namespace _11._The_Party_Reservation_Filter_Module
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<string> names = Console.ReadLine().Split().ToList();
+            List<KeyValuePair<string,string>> commands = new List<KeyValuePair<string,string>>();
+
+            string command = Console.ReadLine();
+
+            while (command != "Print")
+            {
+                string[] cmdArgs = command.Split(";");
+                string cmd = cmdArgs[0];
+                string key = cmdArgs[1];
+                string value = cmdArgs[2];
+
+                if (cmd == "Add filter")
+                {
+                    commands.Add(new KeyValuePair<string, string>(key, value));
+                }
+                else // remove filter
+                {
+
+                    commands.Remove(new KeyValuePair<string, string>(key, value));
+                }
+
+
+                command = Console.ReadLine();
+            }
+
+            Func<List<string>, string, string, List<string>> filterFunc = (list, cmd, argument) =>
+            {
+                if (cmd == "Starts with")
+                {
+                    return list.Where(n => !n.StartsWith(argument)).ToList();
+                }
+                else if (cmd == "Ends with")
+                {
+                    return list.Where(n => !n.EndsWith(argument)).ToList();
+                }
+                else if (cmd == "Length")
+                {
+                    return list.Where(n => n.Length != int.Parse(argument)).ToList();
+                }
+                else if (cmd == "Contains")
+                {
+                    return list.Where(n => !n.Contains(argument)).ToList();
+                }
+                else
+                {
+                    return list;
+                }
+            };
+
+            foreach (var kvp in commands)
+            {
+                names = filterFunc(names, kvp.Key, kvp.Value);
+            }
+
+            Console.WriteLine(string.Join(" ", names));
         }
     }
 }
---
+++ Advanced/Functional Programming - Exercises/12. TriFunction/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+
+namespace _12._TriFunction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int limit = int.Parse(Console.ReadLine());
+            string[] names = Console.ReadLine().Split();
+
+            Func<string, int, bool> filterFunc = (name, limit) =>
+            {
+                if (name.ToCharArray().Sum(c => c) >= limit)
+                {
+                    return true;
+                }
+
+                return false;
+            };
+
+            foreach (var name in names)
+            {
+                if (filterFunc(name, limit))
+                {
+                    Console.WriteLine(name);
+                    break;
+                }
+            }
+        }
+    }
+}
