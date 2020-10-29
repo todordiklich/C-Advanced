dec3cfe10bbeaf11797df5f74f0636be11575dfd
Todor Diklich <todordiklich@gmail.com>
2020-10-29 18:23:22 +0200

    Inheritance - Lab

---
+++ OOP/01. Inheritance - Lab/CustomRandomList/RandomList.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomRandomList
+{
+    public class RandomList : List<string>
+    {
+        private Random rand = new Random();
+
+        public string RandomString()
+        {
+            if (this.Count > 0)
+            {
+                int index = rand.Next(0, this.Count);
+                string str = this[index];
+                this.RemoveAt(index);
+
+                return str;
+            }
+
+            return null;
+        }
+    }
+}
