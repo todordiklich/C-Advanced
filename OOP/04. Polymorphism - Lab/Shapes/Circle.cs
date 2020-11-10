c9809aee9f04045a4c20fc0566ff9ab043b472a4
todordiklich <todordiklich@gmail.com>
2020-11-10 12:51:19 +0200

    Polymorphim - Lab

---
+++ OOP/04. Polymorphism - Lab/Shapes/Circle.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Shapes
+{
+    public class Circle : Shape
+    {
+        public Circle(double radius)
+        {
+            this.Radius = radius;
+        }
+
+        public double Radius { private get; set; }
+
+        public override double CalculateArea()
+        {
+            return Math.PI * this.Radius * this.Radius;
+        }
+
+        public override double CalculatePerimeter()
+        {
+            return 2 * Math.PI * this.Radius;
+        }
+
+        public override string Draw()
+        {
+            return base.Draw() + this.GetType().Name;
+        }
+    }
+}
---
+++ OOP/04. Polymorphism - Lab/Shapes/Rectangle.cs
@@ -0,0 +1,29 @@
+﻿namespace Shapes
+{
+    public class Rectangle : Shape
+    {
+        public Rectangle(double lenght, double width)
+        {
+            this.Lenght = lenght;
+            this.Width = width;
+        }
+
+        public double Lenght { get; private set; }
+        public double Width { get; private set; }
+
+        public override double CalculateArea()
+        {
+            return this.Lenght * this.Width;
+        }
+
+        public override double CalculatePerimeter()
+        {
+            return 2 * this.Lenght + 2 * this.Width;
+        }
+
+        public override string Draw()
+        {
+            return base.Draw() + this.GetType().Name;
+        }
+    }
+}
