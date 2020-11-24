0a579d7860825ef489b82a6f7f1f67cf40a4984b
Todor Diklich <todordiklich@gmail.com>
2020-11-24 21:46:43 +0200

    Unit Testing - Lab

--- OOP/09. Unit Testing - Lab/Skeleton.Tests/AxeTests.cs
+++ OOP/09. Unit Testing - Lab/Skeleton.Tests/AxeTests.cs
@@ -1,11 +1,31 @@
 using NUnit.Framework;
+using System;
 
 [TestFixture]
 public class AxeTests
 {
     [Test]
-    public void Test1()
+    public void AxeLoosesDurabilityAfterAttack()
     {
+        //Arrange
+        Axe axe = new Axe(10, 10);
+        Dummy dummy = new Dummy(20, 20);
 
+        //Act
+        axe.Attack(dummy);
+
+        //Assert
+        Assert.AreEqual(axe.DurabilityPoints, 9);
+    }
+
+    [Test]
+    public void AxeIsBroken()
+    {
+        //Arrange
+        Axe axe = new Axe(10, 0);
+        Dummy dummy = new Dummy(20, 20);
+
+        //Act - Assert
+        Assert.Throws<InvalidOperationException>(() => axe.Attack(dummy));
     }
 }
--- OOP/09. Unit Testing - Lab/Skeleton.Tests/DummyTests.cs
+++ OOP/09. Unit Testing - Lab/Skeleton.Tests/DummyTests.cs
@@ -1,11 +1,53 @@
 ﻿using NUnit.Framework;
+using System;
 
 [TestFixture]
 public class DummyTests
 {
     [Test]
-    public void Test1()
+    public void DummyLooseHealthWhenAttacked()
     {
+        //Arrange
+        Dummy dummy = new Dummy(20, 20);
 
+        //Act
+        dummy.TakeAttack(10);
+
+        //Assert
+        Assert.AreEqual(10, dummy.Health);
+    }
+
+    [Test]
+    public void DummyThrowsExceptionhWhenAttacked()
+    {
+        //Arrange
+        Dummy dummy = new Dummy(0, 20);
+
+        //Act - Assert
+        Assert.Throws<InvalidOperationException>(() => dummy.TakeAttack(10));
+    }
+
+    [Test]
+    public void DummyGivesExperiance()
+    {
+        //Arrange
+        Dummy dummy = new Dummy(0, 20);
+
+        //Act
+        int expectedResult = 20;
+        int actualResult = dummy.GiveExperience();
+
+        //Assert
+        Assert.AreEqual(expectedResult, actualResult);
+    }
+
+    [Test]
+    public void DummyDoesntGivesExperiance()
+    {
+        //Arrange
+        Dummy dummy = new Dummy(20, 20);
+
+        //Act - Assert
+        Assert.Throws<InvalidOperationException>(() => dummy.GiveExperience());
     }
 }
