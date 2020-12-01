53353793763f7bcb708290f469e016c4b65df750
todordiklich <todordiklich@gmail.com>
2020-12-01 13:42:03 +0200

    Design patterns - Singleton demo

---
+++ OOP/11. Design Patterns - Lab/SingletonDemo/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+using SingletonDemo.Models;
+
+namespace SingletonDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Singleton class demo
+            var db = SingletonDataContainer.Instance;
+            var db2 = SingletonDataContainer.Instance;
+            var db3 = SingletonDataContainer.Instance;
+
+            Console.WriteLine(db.GetPopulation("Gabrovo"));
+            Console.WriteLine(db2.GetPopulation("Sevlievo"));
+            Console.WriteLine(db3.GetPopulation("Sofia"));
+
+            //Static class demo
+            StaticDemo.Number = 5;
+            Console.WriteLine(StaticDemo.Number);
+        }
+    }
+}
